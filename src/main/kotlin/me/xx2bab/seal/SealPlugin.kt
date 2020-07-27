a55c82210aba679b52d18364ee2fb4d7bf25d4ba
2BAB <xx2bab@gmail.com>
2020-07-27 21:30:54 +0800

    refactor: resolved #6 migrate all buildscripts to kts, add `buildSrc/` and `kotlin/` source code dir

---
+++ src/main/kotlin/me/xx2bab/seal/SealPlugin.kt
@@ -0,0 +1,12 @@
+package me.xx2bab.seal
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SealPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+
+    }
+
+}
